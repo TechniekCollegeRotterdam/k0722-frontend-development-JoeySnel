0337938b0a1fa8b600dac46a9bd265b8df01cc14
JoeySnel <9016797@student.zadkine.nl>
2022-02-18 09:11:02 +0100

    document outline

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joey Snel | Software Developer 
+    </title>
+</head>
+
+<body>
+    <article>
+        <header>
+            <?php include "nav.html" ?>
+            <h1>Joey Snel</h1>
+            <p>Portfolio</p>
+        </header>
+    </article>
+    <article class="about">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab labore cum, porro reprehenderit quibusdam odit
+            magni perferendis enim delectus voluptas veniam rerum qui dignissimos atque beatae, eos pariatur,
+            voluptatibus reiciendis!</p>
+        <img src="#">
+    </article>
+    <article class="work">
+        <img src="#">
+        <a href="">Bezoek website</a>
+        <img src="#">
+        <a href="">Bezoek website</a>
+    </article>
+    <articel class="skills">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, odio repudiandae labore quos neque
+            praesentium facilis asperiores doloremque blanditiis sint ipsa eius dignissimos animi obcaecati eveniet,
+            libero dolorum debitis sit.</p>
+    </articel>
+    <article>
+        <form>
+            <input type="text" name="mail" placeholder="E-mail">
+            <input type="text" name="name" placeholder="Naam">
+            <textarea name="message" placeholder="Typ hier uw bericht"></textarea>
+            <button type="submit" name="send">Verzenden</button>
+        </form>
+    </article>
+    <article>
+        <footer>
+            <a href="#">GitHub</a>
+            <a href="#">Linkdln</a>
+            <a href="#">@e-mail.com</a>
+        </footer>
+    </article>
+</body>
+
+</html>
